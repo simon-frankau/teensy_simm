74e5feb1619e53bc1fc452abea2189f075778068
Simon Frankau <sgf@arbitrary.name>
2021-07-23 08:28:46 +0100

    Parse the log entries
    One corrupted entry removed. Really annoying that I didn't record the
    full set of corrupted locations, and count the corrupted byte
    sub-locations, but it took ages to collect the data, and I'll do what
    I can with the data I have!

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -4,11 +4,18 @@
 // cells.
 //
 
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
 use std::env;
 use std::fs;
 
 #[derive(Clone, Debug)]
 struct Entry {
+       delay: usize,
+       corrupted: Vec<String>,
+       bit_count: usize,
 }
 
 fn to_entry(s: &str) -> Entry {
@@ -19,7 +26,41 @@ fn to_entry(s: &str) -> Entry {
     // the file.
     assert!(entry.len() == 3 || (entry.len() == 4 && entry[3] == ""));
 
-    Entry{}
+    // First line pattern is "Delay: n, Pattern: m", only care about n.
+    let delay = {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^Delay: ([0-9]*), Pattern: [0-9]*$").unwrap();
+        }
+        let captures = RE.captures(entry[0]).unwrap();
+        captures.get(1).unwrap().as_str().parse::<usize>().unwrap()
+    };
+
+    // Second line is comma-separated list of corrupt locations.
+    // Collect them all. Annoyingly, only the first 31 get recorded.
+    let locations = {
+        let mut locs = entry[1].split(",").map(String::from).collect::<Vec<String>>();
+        // Locations are comma-terminated, so we can always drop the
+        // last entry (empty string).
+        let last = locs.pop().unwrap();
+        assert!(last.is_empty());
+        locs
+    };
+
+    // Third line is number of diffs. 'Diffs' is the number of bit
+    // changes, so if multiple bits are corrupt in the same byte, the
+    // counts may differ.
+    //
+    let num_diffs = {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^Diffs: ([0-9]*)$").unwrap();
+        }
+        let captures = RE.captures(entry[2]).unwrap();
+        captures.get(1).unwrap().as_str().parse::<usize>().unwrap()
+    };
+
+    assert!(locations.len() == 31 || num_diffs == locations.len());
+
+    Entry{ delay: delay, corrupted: locations, bit_count: num_diffs }
 }
 
 fn main() {
